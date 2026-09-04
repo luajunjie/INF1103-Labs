d8a25a05794e15a6db2ff765dd91760e4280b11a
Lua Jun Jie <2602258@sit.singaporetech.edu.sg>
2026-09-04 11:54:59 +0800

    First Post

--- INF1103-Labs/hello.py
+++ INF1103-Labs/hello.py
@@ -1,2 +1,10 @@
-print("test")
+print("This is my first post!")
 print("SIT")
+
+username = "shifool"
+bio = "Retired gamer"
+followers = 1
+
+print("Username:", username)
+print("Bio:", bio)
+print("Followers:", followers)
