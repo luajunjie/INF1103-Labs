bb59e59843bfa24500661ded07cc4c4a94fd2f18
Lua Jun Jie <2602258@sit.singaporetech.edu.sg>
2026-09-04 17:04:31 +0800

    FirstPost

--- INF1103-Labs/hello.py
+++ INF1103-Labs/hello.py
@@ -1,16 +1,20 @@
+# Activity 1
+
 print("This is my first post!")
 print("SIT")
 
+# Activity 2
 username = input("Enter Username: ")
-age = input("Enter Age:")
-bio = print("LOL")
+age = int(input("Enter Age:"))
+bio = "0711 Boi here!"
 category = input("Enter Content Category: ")
-followers = 1
+followers = 134
 
 print("Username:", username)
 print("Bio:", bio)
 print("Followers:", followers)
 
+# Activity 3
 followers += 2
 print("Day 1:", followers)
 
@@ -20,9 +24,13 @@ print("Day 2:", followers)
 followers +=10
 print("Day 3:", followers)
 
+# Activity 4
 print("\ninstagram Profile")
 print("=============")
 print("Username:", username)
 print("Age:", age)
 print("Category:", category)
 
+# Activity 5
+if age>40 and category == "fun":
+    print("what are you doing on instagram??? Are you a pedophile?")
