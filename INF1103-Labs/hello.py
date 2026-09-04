b3084eb2bc2e6d14d69c988f05241834d52176b6
Lua Jun Jie <2602258@sit.singaporetech.edu.sg>
2026-09-04 12:03:11 +0800

    MyFirstPost

--- INF1103-Labs/hello.py
+++ INF1103-Labs/hello.py
@@ -1,8 +1,10 @@
 print("This is my first post!")
 print("SIT")
 
-username = "shifool"
-bio = "Retired gamer"
+username = input("Enter Username: ")
+age = input("Enter Age:")
+bio = print("LOL")
+category = input("Enter Content Category: ")
 followers = 1
 
 print("Username:", username)
@@ -17,3 +19,10 @@ print("Day 2:", followers)
 
 followers +=10
 print("Day 3:", followers)
+
+print("\ninstagram Profile")
+print("=============")
+print("Username:", username)
+print("Age:", age)
+print("Category:", category)
+
