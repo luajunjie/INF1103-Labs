c4884cda339074e16dc6d22e832dc76c40b943f1
Lua Jun Jie <2602258@sit.singaporetech.edu.sg>
2026-09-04 11:57:19 +0800

    MyFirstPost

--- INF1103-Labs/hello.py
+++ INF1103-Labs/hello.py
@@ -8,3 +8,12 @@ followers = 1
 print("Username:", username)
 print("Bio:", bio)
 print("Followers:", followers)
+
+followers += 2
+print("Day 1:", followers)
+
+followers -=3
+print("Day 2:", followers)
+
+followers +=10
+print("Day 3:", followers)
